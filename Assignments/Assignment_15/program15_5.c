1c3fa6fddd628d8f5ffcf86838b0e709800e12bb
omkar-pinjarkar <omkarpinjarkar2004@gmail.com>
2025-11-02 14:38:14 +0530

    Update program15_5.c

--- Assignments/Assignment_15/program15_5.c
+++ Assignments/Assignment_15/program15_5.c
@@ -6,7 +6,7 @@
 //  Description :   To get difference between summation of even and odd frequnecies
 //  Input :         int
 //  Output :        int
-//  Author :        Prajwal Sanjay Pansare
+//  Author :        Omkar Nandkishor Pinjarkar
 //  Date :          27/10/2025
 //
 //////////////////////////////////////////////////////////////
