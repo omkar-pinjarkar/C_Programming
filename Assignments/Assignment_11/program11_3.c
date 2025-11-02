ec2a2603f3ee08a784cf13b6d5a13ed2c1a89e00
omkar-pinjarkar <omkarpinjarkar2004@gmail.com>
2025-11-02 14:33:48 +0530

    Update program11_3.c

--- Assignments/Assignment_11/program11_3.c
+++ Assignments/Assignment_11/program11_3.c
@@ -6,7 +6,7 @@
 //  Description :   To display sum of all numbers between range
 //  Input :         int, int
 //  Output :        int
-//  Author :        Prajwal Sanjay Pansare
+//  Author :        Omkar Nandkishor Pinjarkar
 //  Date :          27/10/2025
 //
 //////////////////////////////////////////////////////////////
