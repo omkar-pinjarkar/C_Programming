e7abd499fe304d3e60d46162dac9a38c33673708
omkar-pinjarkar <omkarpinjarkar2004@gmail.com>
2025-11-02 14:32:50 +0530

    Update program10_3.c

--- Assignments/Assignment_10/program10_3.c
+++ Assignments/Assignment_10/program10_3.c
@@ -6,7 +6,7 @@
 //  Description :   To convert distance kilometer into meter
 //  Input :         int
 //  Output :        int
-//  Author :        Prajwal Sanjay Pansare
+//  Author :        Omkar Nandkishor Pinjarkar
 //  Date :          27/10/2025
 //
 //////////////////////////////////////////////////////////////
