ea120eebeaa52c1806e85f9a69981cb72894c9fe
omkar-pinjarkar <omkarpinjarkar2004@gmail.com>
2025-11-02 14:32:04 +0530

    Update program9_5.c

--- Assignments/Assignment_09/program9_5.c
+++ Assignments/Assignment_09/program9_5.c
@@ -6,7 +6,7 @@
 //  Description :   To get facotrial differcne of input
 //  Input :         int
 //  Output :        int
-//  Author :        Prajwal Sanjay Pansare
+//  Author :        Omkar Nandkishor Pinjarkar
 //  Date :          27/10/2025
 //
 //////////////////////////////////////////////////////////////
