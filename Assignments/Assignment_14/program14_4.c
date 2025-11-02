ce2f9c1398d7109a09e889489a71730d11de56da
omkar-pinjarkar <omkarpinjarkar2004@gmail.com>
2025-11-02 14:37:06 +0530

    Update program14_4.c

--- Assignments/Assignment_14/program14_4.c
+++ Assignments/Assignment_14/program14_4.c
@@ -6,7 +6,7 @@
 //  Description :   To check frequency of 4 in number
 //  Input :         int
 //  Output :        void
-//  Author :        Prajwal Sanjay Pansare
+//  Author :        Omkar Nandkishor Pinjarkar
 //  Date :          27/10/2025
 //
 //////////////////////////////////////////////////////////////
