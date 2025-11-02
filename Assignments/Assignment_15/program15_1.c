fc0fe0480ebcbffa09e1880ac620a91ab8ffd8bb
omkar-pinjarkar <omkarpinjarkar2004@gmail.com>
2025-11-02 14:37:33 +0530

    Update program15_1.c

--- Assignments/Assignment_15/program15_1.c
+++ Assignments/Assignment_15/program15_1.c
@@ -6,7 +6,7 @@
 //  Description :   To check frequency of even digits in number
 //  Input :         int
 //  Output :        int
-//  Author :        Prajwal Sanjay Pansare
+//  Author :        Omkar Nandkishor Pinjarkar
 //  Date :          27/10/2025
 //
 //////////////////////////////////////////////////////////////
