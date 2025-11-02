0eb87b68ba50b6953adc033cb569119b03bc9e90
omkar-pinjarkar <omkarpinjarkar2004@gmail.com>
2025-11-02 14:18:29 +0530

    Update program4_2.c

--- Assignments/Assignment_04/program4_2.c
+++ Assignments/Assignment_04/program4_2.c
@@ -6,7 +6,7 @@
 //  Description :   Display factors in decreasing order
 //  Input :         int
 //  Output :        string
-//  Author :        Prajwal Sanjay Pansare
+//  Author :        Omkar Nandkishor Pinjarkar
 //  Date :          25/10/2025
 //
 //////////////////////////////////////////////////////////////
