86c4aace4c93ea9486d61706f5f6114ff0cc9e07
omkar-pinjarkar <omkarpinjarkar2004@gmail.com>
2025-11-02 14:17:36 +0530

    Update program3_2.c

--- Assignments/Assignment_03/program3_2.c
+++ Assignments/Assignment_03/program3_2.c
@@ -6,7 +6,7 @@
 //  Description :   print even factors of number
 //  Input :         int
 //  Output :        string
-//  Author :        Prajwal Sanjay Pansare
+//  Author :        Omkar Nandkishor Pinjarkar
 //  Date :          25/10/2025
 //
 //////////////////////////////////////////////////////////////
