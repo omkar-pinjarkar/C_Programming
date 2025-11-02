4c7a8c9ce3c8281247a34dbad41e9fd143abd768
omkar-pinjarkar <omkarpinjarkar2004@gmail.com>
2025-11-02 14:30:11 +0530

    Update program8_2.c

--- Assignments/Assignment_08/program8_2.c
+++ Assignments/Assignment_08/program8_2.c
@@ -6,7 +6,7 @@
 //  Description :   convert single digit number into word
 //  Input :         int
 //  Output :        void
-//  Author :        Prajwal Sanjay Pansare
+//  Author :        Omkar Nandkishor Pinjarkar
 //  Date :          26/10/2025
 //
 //////////////////////////////////////////////////////////////
