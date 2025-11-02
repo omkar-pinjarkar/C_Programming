6e62d16912f7afc9f8585e664a04457bb20179b1
omkar-pinjarkar <omkarpinjarkar2004@gmail.com>
2025-11-02 14:32:31 +0530

    Update program10_2.c

--- Assignments/Assignment_10/program10_2.c
+++ Assignments/Assignment_10/program10_2.c
@@ -6,7 +6,7 @@
 //  Description :   To get area of the Rectangle
 //  Input :         float, float
 //  Output :        float
-//  Author :        Prajwal Sanjay Pansare
+//  Author :        Omkar Nandkishor Pinjarkar
 //  Date :          27/10/2025
 //
 //////////////////////////////////////////////////////////////
