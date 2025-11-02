84c61764bc603461dc9fb211d2941af6947bd130
omkar-pinjarkar <omkarpinjarkar2004@gmail.com>
2025-11-02 14:17:54 +0530

    Update program3_4.c

--- Assignments/Assignment_03/program3_4.c
+++ Assignments/Assignment_03/program3_4.c
@@ -6,7 +6,7 @@
 //  Description :   convert case of the input character
 //  Input :         char
 //  Output :        string
-//  Author :        Prajwal Sanjay Pansare
+//  Author :        Omkar Nandkishor Pinjarkar
 //  Date :          25/10/2025
 //
 //////////////////////////////////////////////////////////////
