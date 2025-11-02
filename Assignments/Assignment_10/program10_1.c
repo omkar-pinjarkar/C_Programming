c27833a5f0e24d9501bd99c48b299d7b7af38285
omkar-pinjarkar <omkarpinjarkar2004@gmail.com>
2025-11-02 14:32:18 +0530

    Update program10_1.c

--- Assignments/Assignment_10/program10_1.c
+++ Assignments/Assignment_10/program10_1.c
@@ -6,7 +6,7 @@
 //  Description :   To get area of the circle
 //  Input :         float
 //  Output :        float
-//  Author :        Prajwal Sanjay Pansare
+//  Author :        Omkar Nandkishor Pinjarkar
 //  Date :          27/10/2025
 //
 //////////////////////////////////////////////////////////////
