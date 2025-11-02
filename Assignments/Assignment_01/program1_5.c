e04eae0295ea9a2a3c4f3f27ffba86e18170a45a
omkar-pinjarkar <omkarpinjarkar2004@gmail.com>
2025-11-02 14:15:57 +0530

    Update program1_5.c

--- Assignments/Assignment_01/program1_5.c
+++ Assignments/Assignment_01/program1_5.c
@@ -6,7 +6,7 @@
 //  Description :   Accept the number from user and Print Stars
 //  Input :         int
 //  Output :        string
-//  Author :        Prajwal Sanjay Pansare
+//  Author :        Omkar Nandkishor Pinjarkar
 //  Date :          25/10/2025
 //
 //////////////////////////////////////////////////////////////
